982edd4f5a57c59c62d591597f0aa38c71538a7a
iBobbyTS <ibobbyts@gmail.com>
2024-11-29 13:02:13 -0700

    Change cookie requirement

--- src/main/java/edu/ucalgary/ensf480/group18/user/model/Cookie.java
+++ src/main/java/edu/ucalgary/ensf480/group18/user/model/Cookie.java
@@ -1,12 +1,12 @@
 package edu.ucalgary.ensf480.group18.user.model;
 
-// Need to have the following columns:
+// Databse need to have the following columns:
 // userToken as the unique key in the database
 // addDate for the token to expire
 // userEmail as the foreign key
 
 // Implement the following functions:
-// addRow(username, currentDate, userToken)
+// addRow(username, currentDate), generate the cookie, 16 digit random number and letter
 // getAddDate(token)
 // getUserEmail(token)
 // I'll implement other functions to interact with the frontend
